269f2f8f4e603921e08e4f5cbc8ae9bf28d0a24a
vietdevne <vietnq812@gmail.com>
2024-01-17 22:52:06 +0700

    define return type value for isAdmin and isUser function

--- VietNQ/ex4/ex4.ts
+++ VietNQ/ex4/ex4.ts
@@ -38,20 +38,20 @@ export const persons: Person[] = [
   { type: 'admin', name: 'Bruce Willis', age: 64, role: 'World saver' }
 ];
 
-export function isAdmin(person: Person) {
+export function isAdmin(person: Person): person is Admin {
   return person.type === 'admin';
 }
 
-export function isUser(person: Person) {
+export function isUser(person: Person): person is User {
   return person.type === 'user';
 }
 
 export function logPerson(person: Person) {
   let additionalInformation: string = '';
-  if (isAdmin(person) && 'role' in person) {
+  if (isAdmin(person)) {
       additionalInformation = person.role;
   }
-  if (isUser(person) && 'occupation' in person) {
+  if (isUser(person)) {
       additionalInformation = person.occupation;
   }
   console.log(` - ${person.name}, ${person.age}, ${additionalInformation}`);
