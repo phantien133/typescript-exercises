cdaabe5f6102bc0d2b5e807a71f8f545b4e31812
vietdevne <vietnq812@gmail.com>
2024-01-19 14:19:29 +0700

    ex12/vietNQ

---
+++ VietNQ/ex12/ex12.ts
@@ -0,0 +1,9 @@
+declare module 'stats' {
+  export function getMaxIndex(input: any, comparator: any): any;
+  export function getMaxElement(input: any, comparator: any): any;
+  export function getMinIndex(input: any, comparator: any): any;
+  export function getMinElement(input: any, comparator: any): any;
+  export function getMedianIndex(input: any, comparator: any): any;
+  export function getMedianElement(input: any, comparator: any): any;
+  export function getAverageValue(input: any, comparator: any): any;
+}
